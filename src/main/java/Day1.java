64fa8ef993b6e6e7e9ac4d254abef0986f898a27
Henrik Ygge <henrik@meepo.se>
2023-11-27 20:36:57 +0100

    setup project

---
+++ src/main/java/Day1.java
@@ -0,0 +1,17 @@
+import util.Util;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day1 {
+
+    public static void main(String[] args) {
+        var inputs = Util.readStrings();
+        Util.submitPart1(part1(inputs));
+    }
+
+    private static int part1(List<String> inputs) {
+        return ;
+    }
+}
