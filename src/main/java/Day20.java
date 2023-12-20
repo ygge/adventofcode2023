0c1e0d3107ef5559b2dc9cb08089fc25e519106e
Henrik Ygge <henrik.ygge@lumera.com>
2023-12-20 09:43:59 +0100

    solved day 20 part 2

--- src/main/java/Day20.java
+++ src/main/java/Day20.java
@@ -5,6 +5,7 @@ import java.util.*;
 public class Day20 {
 
     public static void main(String[] args) {
+        Util.verifySubmission();
         var input = Util.readStrings();
         //Util.submitPart1(part1(input));
         Util.submitPart2(part2(input));
@@ -25,6 +26,7 @@ public class Day20 {
             conn.put(fromName, Arrays.stream(to).toList());
         }
 
+        modules.put("rx", new Module("rx"));
         for (Map.Entry<String, List<String>> entry : conn.entrySet()) {
             for (String target : entry.getValue()) {
                 if (modules.containsKey(target)) {
@@ -33,14 +35,45 @@ public class Day20 {
             }
         }
 
-        for (long i = 0; ; ++i) {
-            int count = 0;
+        var target = modules.get("rx").lastReceived.keySet().stream().findFirst().orElseThrow();
+        List<Integer> data = new ArrayList<>();
+        for (String s : modules.get(target).lastReceived.keySet()) {
+            data.add(findLowBit(input, s));
+        }
+        // 3821, 4019, 3923, 3919
+        return -1;
+    }
+
+    private static int findLowBit(List<String> input, String goal) {
+        Map<String, Module> modules = new HashMap<>();
+        Map<String, List<String>> conn = new HashMap<>();
+        for (String row : input) {
+            var s = row.split(" -> ");
+            var from = s[0];
+            var to = s[1].split(", ");
+            var fromName = from;
+            if (from.charAt(0) == '%' || from.charAt(0) == '&') {
+                fromName = from.substring(1);
+            }
+            modules.put(fromName, new Module(from));
+            conn.put(fromName, Arrays.stream(to).toList());
+        }
+
+        modules.put("rx", new Module("rx"));
+        for (Map.Entry<String, List<String>> entry : conn.entrySet()) {
+            for (String target : entry.getValue()) {
+                if (modules.containsKey(target)) {
+                    modules.get(target).lastReceived.put(entry.getKey(), false);
+                }
+            }
+        }
+        for (int count = 0; ; ++count) {
             var states = new LinkedList<State>();
             states.push(new State(null, "broadcaster", false));
             while (!states.isEmpty()) {
                 var state = states.pop();
-                if (Objects.equals(state.name, "rx") && !state.high) {
-                    ++count;
+                if (Objects.equals(state.name, goal) && !state.high) {
+                    return count + 1;
                 }
                 Module module = modules.get(state.name);
                 if (module == null) {
@@ -59,19 +92,13 @@ public class Day20 {
                     module.lastReceived.put(state.from, state.high);
                     sendHigh = !module.lastReceived.values().stream().allMatch(v -> v);
                 }
-                for (String s : conn.get(state.name)) {
-                    states.addLast(new State(state.name, s, sendHigh));
+                if (conn.containsKey(state.name)) {
+                    for (String s : conn.get(state.name)) {
+                        states.addLast(new State(state.name, s, sendHigh));
+                    }
+                } else {
+                    System.out.println(state.name);
                 }
-            }
-            System.out.println();
-            System.out.println("After step " + (i+1));
-            for (Module module : modules.values()) {
-                if (module.on || module.lastReceived.values().stream().anyMatch(v -> v)) {
-                    System.out.println(module.type + " " + module.on + " " + module.lastReceived);
-                }
-            }
-            if (count == 1) {
-                return i + 1;
             }
         }
     }
